d663b3850456360e07ec95750655b6b8da54d6bb
God'sFavour Omoare <godsfavourtolu@gmail.com>
2023-07-31 17:09:45 +0100

    Adding more functionalities and editing the ReadMe file

--- src/Pages/EditNotes.js
+++ src/Pages/EditNotes.js
@@ -1,24 +1,76 @@
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
 import { IoIosArrowBack } from "react-icons/io";
-import {RiDeleteBin6Line} from "react-icons/ri"
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { useState } from "react";
+import setDate from "../components/SetDate";
 
-const EditNotes = ({notes, setNotes}) => {
-    const {id} = useParams()
-    console.log(id)
-     
+const EditNotes = ({ notes, setNotes }) => {
+  const { id } = useParams();
+  const note = notes.find((item) => item.id === id);
+  const [title, setTitle] = useState(note.title);
+  const [details, setDetails] = useState(note.details);
+  const date = setDate();
+  const navigate = useNavigate();
+
+  const handleForm = (e) => {
+    e.preventDefault();
+
+    if (title && details) {
+      const newNote = { ...note, title, details, date };
+
+      const newNotes = notes.map((item) => {
+        if (item.id === id) {
+          item = newNote;
+        }
+        return item;
+      });
+      setNotes(newNotes);
+    }
+
+    // redirect to home page
+    navigate("/");
+  };
+
+  const handleDelete = () => {
+    if (window.confirm("Are you sure you want to delete?")) {
+      const noteDelete = notes.filter((item) => item.id !== id);
+
+      setNotes(noteDelete);
+    }
+    navigate("/");
+  };
+
   return (
     <section>
       <header className="create-notes__header">
         <Link to={"/"} className="btn">
           <IoIosArrowBack />
         </Link>
-        <button className="btn btn_save">Save</button>
-        <button className="btn btn_delete"><RiDeleteBin6Line /></button>
+
+        <button onClick={handleForm} className="btn btn_save">
+          Save
+        </button>
+
+        <button onClick={handleDelete} className="btn btn_delete">
+          <RiDeleteBin6Line />
+        </button>
       </header>
 
-      <form className="create-note__form">
-        <input type="text" autoFocus placeholder="Title"/>
-        <textarea rows="25" placeholder="Note details..."></textarea>
+      <form className="create-note__form" onSubmit={handleForm}>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          autoFocus
+          placeholder="Title"
+        />
+
+        <textarea
+          rows="25"
+          value={details}
+          onChange={(e) => setDetails(e.target.value)}
+          placeholder="Note details..."
+        ></textarea>
       </form>
     </section>
   );
--- src/Pages/Notes.js
+++ src/Pages/Notes.js
@@ -1,27 +1,64 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { CiSearch } from "react-icons/ci";
-import NoteItem from "../components/NoteItem";
+import { MdClose } from "react-icons/md";
+import NoteItem from "../components/NoteItem"; 
 import { Link } from "react-router-dom";
 import { BsPlusLg } from "react-icons/bs";
 
-const Notes = ({notes}) => {
+const Notes = ({ notes }) => {
+  const [showSearch, setShowSearch] = useState(false);
+  const [text, setText] = useState("");
+  const [filteredNotes, setFilteredNotes] = useState(notes);
+
+  const handleSearch = () => {
+    setFilteredNotes(
+      // eslint-disable-next-line array-callback-return
+      notes.filter((note) => {
+        if (note.title.toLowerCase().match(text.toLocaleLowerCase())) {
+          return note;
+        }
+      })
+    );
+  };
+
+  useEffect(handleSearch, [notes, text]);
+
   return (
     <section>
       <header className="notes_header">
-        <h2>My Notes</h2>
-        {/* <input type="text" autoFocus placeholder="Keyword..." /> */}
-        <button className="btn">
-          <CiSearch />
+        {!showSearch && <h2>My Notes</h2>}
+        {showSearch && (
+          <input
+            type="text"
+            value={text}
+            onChange={(e) => {
+              setText(e.target.value);
+              handleSearch();
+            }}
+            autoFocus
+            placeholder="Keyword..."
+          />
+        )}
+        <button
+          onClick={() => {
+            setShowSearch((prevState) => !prevState);
+          }}
+          className="btn"
+        >
+          {showSearch ? <MdClose /> : <CiSearch />}
         </button>
       </header>
 
       <div className="notes_container">
-        {notes.map((note) => (
+        {filteredNotes.length === 0 && (
+          <p className="empty_note">No notes found</p>
+        )}
+        {filteredNotes.map((note) => (
           <NoteItem key={note.id} note={note} />
         ))}
       </div>
 
-      <Link to={"/create-notes/:id"} className="btn btn__add">
+      <Link to={"/create-notes/"} className="btn btn__add">
         <BsPlusLg />
       </Link>
     </section>
